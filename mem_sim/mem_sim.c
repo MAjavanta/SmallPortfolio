158fd9cc2c15a14b01431234cc5f29d4259d921c
MAjavanta <68334780+MAjavanta@users.noreply.github.com>
2020-07-15 15:17:13 +0100

    mem_sim
    A memory simulator written in C. It simulates how memory is accessed and overwritten. The program is run with 3 parameters: replacement policy, associativity and number of cache blocks.
    Replacement policy is how memory is replaced (i.e. which address it is placed in in the cache block) and can be FIFO (first in first out), LRU (Least Recently Used) and Random.
    Associativity is how wide cache blocks are, and can be any power of 2 up to the number of cache blocks.
    Number of cache blocks can be 16, 64, 256 or 1024.

---
+++ mem_sim/mem_sim.c
@@ -0,0 +1,300 @@
+ /* *    Inf2C-CS Coursework 2: Cache Simulation
+ * *
+ * *    Instructor: Boris Grot
+ * *
+ * *    TA: Siavash Katebzadeh
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <inttypes.h>
+#include <math.h>
+#include <time.h>
+/* Do not add any more header files */
+
+/*
+ * Various structures
+ */
+typedef enum {FIFO, LRU, Random} replacement_p;
+
+const char* get_replacement_policy(uint32_t p) {
+    switch(p) {
+    case FIFO: return "FIFO";
+    case LRU: return "LRU";
+    case Random: return "Random";
+    default: assert(0); return "";
+    };
+    return "";
+}
+
+typedef struct {
+    uint32_t address;
+} mem_access_t;
+
+// These are statistics for the cache and should be maintained by you.
+typedef struct {
+    uint32_t cache_hits;
+    uint32_t cache_misses;
+} result_t;
+
+
+/*
+ * Parameters for the cache that will be populated by the provided code skeleton.
+ */
+
+replacement_p replacement_policy = FIFO;
+uint32_t associativity = 0;
+uint32_t number_of_cache_blocks = 0;
+uint32_t cache_block_size = 0;
+
+
+/*
+ * Each of the variables below must be populated by you.
+ */
+uint32_t g_num_cache_tag_bits = 0;
+uint32_t g_cache_offset_bits= 0;
+result_t g_result;
+
+/* Reads a memory access from the trace file and returns
+ * 32-bit physical memory address
+ */
+mem_access_t read_transaction(FILE *ptr_file) {
+    char buf[1002];
+    char* token = NULL;
+    char* string = buf;
+    mem_access_t access;
+
+    if (fgets(buf, 1000, ptr_file)!= NULL) {
+        /* Get the address */
+        token = strsep(&string, " \n");
+        access.address = (uint32_t)strtoul(token, NULL, 16);
+        return access;
+    }
+
+    /* If there are no more entries in the file return an address 0 */
+    access.address = 0;
+    return access;
+}
+
+void print_statistics(uint32_t num_cache_tag_bits, uint32_t cache_offset_bits, result_t* r) {
+    /* Do Not Modify This Function */
+
+    uint32_t cache_total_hits = r->cache_hits;
+    uint32_t cache_total_misses = r->cache_misses;
+    printf("CacheTagBits:%u\n", num_cache_tag_bits);
+    printf("CacheOffsetBits:%u\n", cache_offset_bits);
+    printf("Cache:hits:%u\n", r->cache_hits);
+    printf("Cache:misses:%u\n", r->cache_misses);
+    printf("Cache:hit-rate:%2.1f%%\n", cache_total_hits / (float)(cache_total_hits + cache_total_misses) * 100.0);
+}
+
+/*
+ *
+ * Add any global variables and/or functions here as needed.
+ *
+ */
+
+typedef enum {hit, miss, first_entry} check_for_hits;
+
+check_for_hits look_for_hits(uint32_t index, uint32_t associativity, uint32_t tag, uint32_t **cache, uint32_t **replacements) {
+    int i;
+    for (i = 0; i < associativity; i++) {
+        if (*(*(cache + index) + i) == 0) {
+            *(*(cache + index) + i) = tag;
+            if (replacement_policy == FIFO) {
+                (*(*(replacements + index)))++;
+            }
+            else if (replacement_policy == LRU) {
+                if (i == 0) {
+                    *(*(replacements + index) + i) = 1;
+                } else {
+                    *(*(replacements + index) + i) = *(*(replacements + index) + (i - 1)) + 1;
+                }
+            } else {
+                *(*(replacements + index) + i) = 0;
+            }
+            return first_entry;
+        } else if (*(*(cache + index) + i) == tag) {
+            if (replacement_policy == LRU) {
+                uint32_t highest = 0;
+                uint32_t counter;
+                for (counter = 0; counter < associativity; counter++) {
+                    if (*(*(replacements + index) + counter) > highest) {
+                        highest = *(*(replacements + index) + counter);
+                    }
+                }
+                *(*(replacements + index) + i) = highest + 1;
+            }
+            return hit;
+
+        }
+    }
+    return miss;
+}
+
+int main(int argc, char** argv) {
+    // uint32_t total_cache_hits = 0;
+    // uint32_t total_cache_misses = 0;
+    time_t t;
+    /* Intializes random number generator */
+    /* Important: *DO NOT* call this function anywhere else. */
+    srand((unsigned) time(&t));
+    /* ----------------------------------------------------- */
+    /* ----------------------------------------------------- */
+
+    /*
+     *
+     * Read command-line parameters and initialize configuration variables.
+     *
+     */
+    int improper_args = 0;
+    char file[10000];
+    if (argc < 6) {
+        improper_args = 1;
+        printf("Usage: ./mem_sim [replacement_policy: FIFO LRU Random] [associativity: 1 2 4 8 ...] [number_of_cache_blocks: 16 64 256 1024] [cache_block_size: 32 64] mem_trace.txt\n");
+    } else  {
+        /* argv[0] is program name, parameters start with argv[1] */
+        if (strcmp(argv[1], "FIFO") == 0) {
+            replacement_policy = FIFO;
+        } else if (strcmp(argv[1], "LRU") == 0) {
+            replacement_policy = LRU;
+        } else if (strcmp(argv[1], "Random") == 0) {
+            replacement_policy = Random;
+        } else {
+            improper_args = 1;
+            printf("Usage: ./mem_sim [replacement_policy: FIFO LRU Random] [associativity: 1 2 4 8 ...] [number_of_cache_blocks: 16 64 256 1024] [cache_block_size: 32 64] mem_trace.txt\n");
+        }
+        associativity = atoi(argv[2]);
+        number_of_cache_blocks = atoi(argv[3]);
+        cache_block_size = atoi(argv[4]);
+        strcpy(file, argv[5]);
+    }
+    if (improper_args) {
+        exit(-1);
+    }
+    assert(number_of_cache_blocks == 16 || number_of_cache_blocks == 64 || number_of_cache_blocks == 256 || number_of_cache_blocks == 1024);
+    assert(cache_block_size == 32 || cache_block_size == 64);
+    assert(number_of_cache_blocks >= associativity);
+    assert(associativity >= 1);
+
+    printf("input:trace_file: %s\n", file);
+    printf("input:replacement_policy: %s\n", get_replacement_policy(replacement_policy));
+    printf("input:associativity: %u\n", associativity);
+    printf("input:number_of_cache_blocks: %u\n", number_of_cache_blocks);
+    printf("input:cache_block_size: %u\n", cache_block_size);
+    printf("\n");
+
+    /* Open the file mem_trace.txt to read memory accesses */
+    FILE *ptr_file;
+    ptr_file = fopen(file,"r");
+    if (!ptr_file) {
+        printf("Unable to open the trace file: %s\n", file);
+        exit(-1);
+    }
+
+    /* result structure is initialized for you. */
+    memset(&g_result, 0, sizeof(result_t));
+
+    /* Do not delete any of the lines below.
+     * Use the following snippet and add your code to finish the task. */
+
+    /* You may want to setup your Cache structure here. */
+    uint32_t rows = number_of_cache_blocks / associativity;
+
+    uint32_t **cache = (uint32_t **)calloc(sizeof(uint32_t) * rows, cache_block_size);
+    uint32_t columns;
+    for (columns = 0; columns < (rows); columns++) {
+        cache[columns] = (uint32_t *)calloc(sizeof(uint32_t) * associativity, cache_block_size);
+    }
+
+    uint32_t **replacements = (uint32_t **)calloc(sizeof(uint32_t) * rows, cache_block_size);
+    uint32_t c;
+    for (c = 0; c < (rows); c++) {
+        replacements[c] = (uint32_t *)calloc(sizeof(uint32_t) * associativity, cache_block_size);
+    }
+
+    g_cache_offset_bits = log2(cache_block_size);
+    uint32_t cache_index_bits = log2(rows);
+    g_num_cache_tag_bits = 32 - (g_cache_offset_bits + cache_index_bits);
+
+    mem_access_t access;
+    /* Loop until the whole trace file has been read. */
+    while(1) {
+        access = read_transaction(ptr_file);
+        // If no transactions left, break out of loop.
+        if (access.address == 0)
+            break;
+
+        /* Add your code here */
+        uint32_t mem_address = access.address;
+        uint32_t tag = mem_address / pow(2, (cache_index_bits + g_cache_offset_bits));
+        uint32_t index = (mem_address - (tag * pow(2, (cache_index_bits + g_cache_offset_bits)))) / pow(2, g_cache_offset_bits);
+
+        check_for_hits result = look_for_hits(index, associativity, tag, cache, replacements);
+        uint32_t counter;
+        if (result == hit) {
+            g_result.cache_hits++;
+        } else if (result == miss) {
+            g_result.cache_misses++;
+
+            if (replacement_policy == LRU) {
+                uint32_t set_index;
+                uint32_t lowest = 1025;
+                uint32_t highest = 0;
+
+                for (counter = 0; counter < associativity; counter++) {
+                    if (*(*(replacements + index) + counter) < lowest) {
+                        lowest = *(*(replacements + index) + counter);
+                        set_index = counter;
+                    }
+                    if (*(*(replacements + index) + counter) > highest) {
+                        highest = *(*(replacements + index) + counter);
+                    }
+                }
+                *(*(cache + index) + set_index) = tag;
+                *(*(replacements + index) + set_index) = highest + 1;
+            } else if (replacement_policy == FIFO) {
+                uint32_t j_index = (*(*(replacements + index))) % associativity;
+                *(*(cache + index) + (j_index)) = tag;
+                (*(*(replacements + index)))++;
+            } else {
+                uint32_t rand_index = 0;
+                do {
+                    rand_index = rand() % associativity;
+                } while (*(*(replacements + index) + rand_index) == 1);
+                *(*(cache + index) + rand_index) = tag;
+                *(*(replacements + index) + rand_index) = 1;
+                uint32_t sum = 0;
+                for (counter = 0; counter < associativity; counter++) {
+                    sum = sum + *(*(replacements + index) + counter);
+                }
+                if (sum == associativity) {
+                    for (counter = 0; counter < associativity; counter++) {
+                        *(*(replacements + index) + counter) = 0;
+                    }
+                }
+            }
+        } else {
+            g_result.cache_misses++;
+        }
+
+    }
+    int row_counter;
+    for (row_counter = 0; row_counter < rows; row_counter++) {
+        free(*(cache + row_counter));
+    }
+    free(cache);
+    for (row_counter = 0; row_counter < rows; row_counter++) {
+        free(*(replacements + row_counter));
+    }
+    free(replacements);
+
+    /* Do not modify code below. */
+    /* Make sure that all the parameters are appropriately populated. */
+    print_statistics(g_num_cache_tag_bits, g_cache_offset_bits, &g_result);
+
+    /* Close the trace file. */
+    fclose(ptr_file);
+    return 0;
